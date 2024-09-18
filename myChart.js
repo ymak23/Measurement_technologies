215ebed6376c08a144d38feb696dfa02f5890fec
Yulia <137413365+ymak23@users.noreply.github.com>
2024-09-18 10:33:50 +0300

    myChart.js

--- myChart.js
+++ myChart.js
@@ -1,21 +1,8 @@
 let ctx = document.getElementById('myChart').getContext('2d');
 let chart = new Chart(ctx, {
-// Тип графика
-type: 'line',
- 
-// Создание графиков
-data: {
-    // Точки графиков
-    labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль'],
-    // График
-    datasets: [{
-        label: 'Мой первый график на Chart.js', // Название
-        backgroundColor: 'rgb(255, 99, 132)', // Цвет закраски
-        borderColor: 'rgb(255, 99, 132)', // Цвет линии
-        data: [0, 10, 5, 2, 20, 30, 45] // Данные каждой точки графика
-    }]
-},
- 
-// Настройки графиков
+
+type: 'line', 
+
+
 options: {}
 });
